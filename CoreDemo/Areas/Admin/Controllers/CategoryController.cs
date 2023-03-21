193769cca881fddf054d0cff3c67a9680e99c7fb
Berkaygulmez <berkaybjk04@icloud.com>
2023-03-21 22:16:33 +0300

    Admin ; Map Kontroller,Kategorilerin listelenmesi, Pagination, Yeni Kategori Eklme, Confirm Delete

--- CoreDemo/Areas/Admin/Controllers/CategoryController.cs
+++ CoreDemo/Areas/Admin/Controllers/CategoryController.cs
@@ -1,13 +1,60 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntitiyLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using X.PagedList;
+using FluentValidation.Results;
 
 namespace CoreDemo.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        CategoryManager cm = new CategoryManager(new EfCategoryRepository());
+        public IActionResult Index(int page = 1)
+        {
+            var values = cm.GetList().ToPagedList(page, 3);
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddCategory()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult AddCategory(Category p)
+        {
+
+            CategoryValidator cv = new CategoryValidator();
+            ValidationResult result = cv.Validate(p);
+            if (result.IsValid)
+            {
+                p.CategoryStatus = true;
+                cm.TAdd(p);
+                return RedirectToAction("Index", "Category");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+        public IActionResult CategoryDelete(int id)
+        {
+            var value = cm.TGetById(id);
+            cm.TDelete(value);
+            return RedirectToAction("Index");
+        }
+
     }
 }
+
--- CoreDemo/Areas/Admin/Controllers/HomeController.cs
+++ CoreDemo/Areas/Admin/Controllers/HomeController.cs
@@ -1,12 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using CoreDemo.Controllers;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class HomeController : Controller
     {
+        CategoryManager cm = new CategoryManager(new EfCategoryRepository());
         public IActionResult Index()
         {
-            return View();
+            var values = cm.GetList();
+            return View(values);
         }
     }
 }
