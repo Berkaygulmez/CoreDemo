29a104b1eaba3d38da9a0c8c4ba7be2f191e8542
Berkaygulmez <berkaybjk04@icloud.com>
2023-03-28 20:50:46 +0300

    Apilere giriş , swagger ve api get list, api ile ,veri ekleme getirme,silme , güncelleme, apilerle projenin haberleştirilmesi list & add

---
+++ CoreDemo/Controllers/EmployeeTestController.cs
@@ -0,0 +1,45 @@
+﻿using DocumentFormat.OpenXml.Office2010.ExcelAc;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    public class EmployeeTestController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var httpClient = new HttpClient();
+            var responseMessage = await httpClient.GetAsync("https://localhost:44365/api/Default");
+            var jsonstring = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<class1>>(jsonstring);
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddEmployee()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee(class1 p)
+        {
+            var httpClient = new HttpClient();
+            var jsonEmployee=JsonConvert.SerializeObject(p);
+            StringContent content = new StringContent(jsonEmployee,Encoding.UTF8,"application/json");
+            var responseMessage = await httpClient.PostAsync("https://localhost:44365/api/Default", content);
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(p);
+        }
+    }
+    public class class1
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }    
+    }
+}
