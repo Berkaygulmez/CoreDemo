078aed076455de25aae93d42080a346451b0f7a0
Berkaygulmez <berkaybjk04@icloud.com>
2023-04-06 19:44:18 +0300

    Yazar Bilgilerinin Identitye Bağlanması,Identity FindByNameAsync ve UpdateAsync

--- CoreDemo/Controllers/WriterController.cs
+++ CoreDemo/Controllers/WriterController.cs
@@ -7,21 +7,31 @@ using EntitiyLayer.Concrete;
 using FluentValidation;
 using FluentValidation.Results;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 //using ValidationResult = FluentValidation.Results.ValidationResult; //bune lan
 
 namespace CoreDemo.Controllers
 {
-    [Authorize]
     public class WriterController : Controller
     {
         WriterManager wm = new WriterManager(new EfWriterRepository());
+        UserManager userManager = new UserManager(new EfUserRepository());
+
+
+        private readonly UserManager<AppUser> _userManager;
+
+        public WriterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
 
         [Authorize]
         public IActionResult Index()
@@ -58,36 +68,28 @@ namespace CoreDemo.Controllers
         }
 
         [HttpGet]
-        public IActionResult WriterEditProfile()
+        public async Task<IActionResult> WriterEditProfile()
         {
-            Context c = new Context();
-            var username = User.Identity.Name;
-            var usermail =c.Users.Where(x=>x.UserName==username).Select(y=>y.Email).FirstOrDefault();
-            var writerID = c.Writers.Where(x => x.Writermail == usermail).Select(y => y.WriterID).FirstOrDefault();
-            var writervaleus = wm.TGetById(writerID);
-            return View(writervaleus);
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            UserUpdateViewModel model = new UserUpdateViewModel();
+            model.mail = values.Email;
+            model.namesurname = values.NameSurname;
+            model.imageurl = values.ImageURL;
+            model.username = values.UserName;
+            return View(model);
         }
 
- 
-        [HttpPost]
-        public IActionResult WriterEditProfile(Writer p)
-        {
 
-            WriterValidator wl = new WriterValidator();
-            ValidationResult result = wl.Validate(p);
-            if (result.IsValid)
-            {
-                wm.TUpdate(p);
-                return RedirectToAction("Index", "Dashboard");
-            }
-            else
-            {
-                foreach (var item in result.Errors)
-                {
-                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
-                }
-            }
-            return View();
+        [HttpPost]
+        public async Task<IActionResult> WriterEditProfile(UserUpdateViewModel model)
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            values.NameSurname = model.namesurname;
+            values.ImageURL = model.imageurl;
+            values.Email = model.mail;
+            var result = await _userManager.UpdateAsync(values);
+            return RedirectToAction("Index", "Dashboard");
+
         }
         [AllowAnonymous]
         [HttpGet]
@@ -100,9 +102,9 @@ namespace CoreDemo.Controllers
         public IActionResult WriterAdd(AddProfileImage p)
         {
             Writer w = new Writer();
-            if (p.WriterImage != null) //sadece burda sormuş foto yüklendimi diye
+            if (p.WriterImage != null)
             {
-                var extension = Path.GetExtension(p.WriterImage.FileName); //bu kodlar foto olmadan çalışmayı denerse hata alır o yüzden yukarda sorup garantiye almış
+                var extension = Path.GetExtension(p.WriterImage.FileName);
                 var newimagename = Guid.NewGuid() + extension;
                 var location = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/WriterImageFiles/", newimagename);
                 var stream = new FileStream(location, FileMode.Create);
@@ -116,10 +118,7 @@ namespace CoreDemo.Controllers
             w.WriterAbout = p.WriterAbout;
             wm.TAdd(w);
 
-            return RedirectToAction("Index", "Dashboard"); //kaydederse dashboarda gidecek
-
-            //var mı başka soru??
-
+            return RedirectToAction("Index", "Dashboard");
         }
     }
 }
